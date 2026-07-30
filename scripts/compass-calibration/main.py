e2944b9b1ebebefaee6a0440898e3208ebeabd4c
Proud Cobol Writer <77924245+proudCobolWriter@users.noreply.github.com>
2026-07-30 23:57:20 +0200

    Add calibration feature to helper script

--- scripts/compass-calibration/main.py
+++ scripts/compass-calibration/main.py
@@ -1,109 +1,327 @@
 import serial.tools.list_ports
-import sys, os
+import threading, time, sys, os
 
+import matplotlib.widgets as widgets
 import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
 
 from queue import Queue
 
-ports = serial.tools.list_ports.comports()
-choices = {}
+# CONSTANTS
 
-BAUD_RATE = 9600
-MAX_POINTS = 5000
+BAUD_RATE = 115200
+MAX_POINTS = 250
 
-for port, desc, hwid in sorted(ports):
-    print(f"{port}: {desc} [{hwid}]")
-    choices[port] = desc
-
-print("*" * os.get_terminal_size()[0])
-
-if len(choices) == 0:
-    print("No COM ports were found, aborting...")
-    sys.exit(0)
-
-choice = input("Choose the COM port you want... ").upper()
-
-if choice.strip() == "":
-    choice = list(choices.keys())[0]
-else:
-    if choice not in choices:
-        if f"COM{choice}" in choices:
-            choice = f"COM{choice}"
-        elif f"/dev/ttyUSB{choice}" in choices:
-            choice = f"/dev/ttyUSB{choice}"
-        else:
-            print("That COM port was not found, aborting...")
-            sys.exit(0)
-
-print(f"Port {choice} was chosen")
-
-x_data = Queue(maxsize=MAX_POINTS)
-y_data = Queue(maxsize=MAX_POINTS)
-z_data = Queue(maxsize=MAX_POINTS)
+# ENDOFCONSTANTS
 
 
+def scanports(portsdict):
+    ports = serial.tools.list_ports.comports()
 
-def display_plot():
-    plt.ion()
+    for port, desc, hwid in sorted(ports):
+        print(f"{port}: {desc} [{hwid}]")
+        portsdict[port] = desc
 
-    global fig, ax
-    fig, ax = plt.subplots()
+    return portsdict
 
-    global x_cloud, y_cloud, z_cloud
-    x_cloud, = ax.plot([], [], "o", markersize=5, label="XY plane", alpha=1.0)
-    y_cloud, = ax.plot([], [], "o", markersize=5, label="YZ plane", alpha=1.0)
-    z_cloud, = ax.plot([], [], "o", markersize=5, label="ZX plane", alpha=1.0)
 
-    ax.set_title("QMC5883L Calibration")
-    ax.legend()
+def printsep():
+    print("*" * os.get_terminal_size()[0])
 
-def update_plot():
-    x_cloud.set_data([coord[0] for coord in x_data.queue], [coord[1] for coord in x_data.queue])
-    y_cloud.set_data([coord[0] for coord in y_data.queue], [coord[1] for coord in y_data.queue])
-    z_cloud.set_data([coord[0] for coord in z_data.queue], [coord[1] for coord in z_data.queue])
 
-    ax.relim()
-    ax.autoscale_view()
-    ax.set_aspect("equal", "box")
+def promptuser(portsdict):
+    choice = input("Choose the COM port you want... ").upper()
 
-    fig.canvas.draw_idle()
-    fig.canvas.flush_events()
+    if choice.strip() == "":
+        choice = list(portsdict.keys())[0]
+    else:
+        if choice not in portsdict:
+            if f"COM{choice}" in portsdict:
+                choice = f"COM{choice}"
+            elif f"/dev/ttyUSB{choice}" in portsdict:
+                choice = f"/dev/ttyUSB{choice}"
+            else:
+                return None
 
-display_plot()
+    return choice
 
-mcu = serial.Serial(choice, BAUD_RATE, timeout=1)
 
-try:
-    while True:
-        if mcu.in_waiting > 0:
-            line = mcu.readline()
-            if not line:
-                continue
+class CompassGUI:
+    def __init__(self, mcu):
+        self.is_running = False
+        self.mcu = mcu
 
-            text = line.decode("utf-8", "ignore").strip()
-            if not text or not text.startswith("Raw:"):
-                continue
+        print("Active MLP backend:", matplotlib.get_backend())
 
-            try:
-                text = text[4:]
+        plt.ion()
 
-                values = text.split(",")
-                if len(values) != 9:
-                    continue
+        self.fig, self.ax = plt.subplots(figsize=(10, 7))
 
-                mag_values = values[6:]
-                x, y, z = [int(v) for v in mag_values]
+        plt.subplots_adjust(left=0.15, bottom=0.2, right=0.85)  # room for the buttons
 
-                x_data.put((x, y))
-                y_data.put((y, z))
-                z_data.put((z, x))
+        self.xy_data = Queue(maxsize=MAX_POINTS)
+        self.yz_data = Queue(maxsize=MAX_POINTS)
+        self.zx_data = Queue(maxsize=MAX_POINTS)
 
-                update_plot()
-            except ValueError:
-                print("Wrong line!")
-                continue
-except KeyboardInterrupt:
-    print("Interrupted, aborting....")
-finally:
-    mcu.close()
+        (self.x_cloud,) = self.ax.plot(
+            [], [], "o", markersize=5, label="XY plane", alpha=1.0
+        )
+        (self.y_cloud,) = self.ax.plot(
+            [], [], "o", markersize=5, label="YZ plane", alpha=1.0
+        )
+        (self.z_cloud,) = self.ax.plot(
+            [], [], "o", markersize=5, label="ZX plane", alpha=1.0
+        )
+
+        self.ax.set_title("QMC5883L Calibration")
+        self.ax.legend()
+
+        pos = self.ax.get_position()
+
+        self.constants = {"btn_width": 0.12, "btn_height": 0.05, "btn_y": 0.05}
+
+        pos_left = pos.x0
+        pos_right = pos.x1 - self.constants["btn_width"]
+        pos_mid = (pos_left + pos_right) / 2
+
+        pos_left = pos_mid - self.constants["btn_width"] * 1.25
+        pos_right = pos_mid + self.constants["btn_width"] * 1.25
+
+        ax_start = self.fig.add_axes(
+            (
+                pos_left,
+                self.constants["btn_y"],
+                self.constants["btn_width"],
+                self.constants["btn_height"],
+            )
+        )
+        ax_reset = self.fig.add_axes(
+            (
+                pos_right,
+                self.constants["btn_y"],
+                self.constants["btn_width"],
+                self.constants["btn_height"],
+            )
+        )
+        ax_stop = self.fig.add_axes(
+            (
+                pos_mid,
+                self.constants["btn_y"],
+                self.constants["btn_width"],
+                self.constants["btn_height"],
+            )
+        )
+
+        self.btn_start = widgets.Button(ax_start, "Start")
+        self.btn_reset = widgets.Button(ax_reset, "Reset")
+        self.btn_stop = widgets.Button(ax_stop, "Stop")
+
+        self.btn_start.on_clicked(self.start)
+        self.btn_reset.on_clicked(self.reset)
+        self.btn_stop.on_clicked(self.stop)
+
+        self.points_text = self.fig.text(
+            (0.75 + 1) / 2,
+            0.525,
+            f"Points:\n0/{MAX_POINTS}",
+            fontsize=10,
+            horizontalalignment="center",
+            verticalalignment="center",
+            family="monospace",
+        )
+
+        self.update()
+
+    def start(self, _):
+        if self.is_running:
+            return
+
+        self.is_running = True
+        print("Started")
+
+        self.thread = threading.Thread(target=self.listen_serial, daemon=True)
+        self.thread.start()
+
+    def stop(self, _):
+        if not self.is_running:
+            return
+
+        self.is_running = False
+        print("Stopped")
+
+    def reset(self, _):
+        print("Resetting points")
+
+        for q in [self.xy_data, self.yz_data, self.zx_data]:
+            with q.mutex:
+                q.queue.clear()
+                q.all_tasks_done.notify_all()
+                q.not_full.notify_all()
+
+        self.update()
+
+    def update(self):
+        self.x_cloud.set_data(
+            [coord[0] for coord in self.xy_data.queue],
+            [coord[1] for coord in self.xy_data.queue],
+        )
+        self.y_cloud.set_data(
+            [coord[0] for coord in self.yz_data.queue],
+            [coord[1] for coord in self.yz_data.queue],
+        )
+        self.z_cloud.set_data(
+            [coord[0] for coord in self.zx_data.queue],
+            [coord[1] for coord in self.zx_data.queue],
+        )
+
+        self.ax.relim()
+        self.ax.autoscale_view()
+        self.ax.set_aspect("equal", "box")
+
+        self.fig.canvas.draw_idle()
+        self.fig.canvas.flush_events()
+
+        self.points_text.set_text(f"Points:\n{self.xy_data.qsize()}/{MAX_POINTS}")
+
+    def listen_serial(self):
+        try:
+            while self.is_running:
+                if self.mcu.in_waiting > 0:
+                    line = self.mcu.readline()
+                    if not line:
+                        continue
+
+                    text = line.decode("utf-8", "ignore").strip()
+                    if not text or not text.startswith("Raw:"):
+                        continue
+
+                    try:
+                        text = text[4:]
+
+                        values = text.split(",")
+                        if len(values) != 9:
+                            continue
+
+                        mag_values = values[6:]
+                        x, y, z = [int(v) for v in mag_values]
+
+                        if hasattr(self, "xCenter"):
+                            x -= self.xCenter
+                            y -= self.yCenter
+                            z -= self.zCenter
+
+                        if self.xy_data.full():
+                            self.xy_data.get()
+                            self.yz_data.get()
+                            self.zx_data.get()
+
+                        self.xy_data.put((x, y))
+                        self.yz_data.put((y, z))
+                        self.zx_data.put((z, x))
+
+                        self.update()
+                    except ValueError:
+                        print("Wrong line!")
+                        continue
+        except serial.SerialException:
+            print("Serial bridge was broken!")
+        finally:
+            print("Worker thread stopped")
+            self.done()
+
+    def queueBounds(self, q):
+        if len(q.queue) == 0:
+            return 0.0, 0.0
+        values = [v[0] for v in q.queue]
+        return min(values), max(values)
+
+    def requeueWOffset(self, q, offset1, offset2):
+        coords_pool = []
+
+        while q.empty():
+            coords_pool.append(q.get())
+
+        for coord in coords_pool:
+            c1, c2 = coord
+            q.put((c1 - offset1, c2 - offset2))
+
+    def done(self):
+        xmin, xmax = self.queueBounds(self.xy_data)
+        ymin, ymax = self.queueBounds(self.yz_data)
+        zmin, zmax = self.queueBounds(self.zx_data)
+
+        printsep()
+        print("Calibration results:")
+        print(
+            f"X enveloppe: {xmin} - {xmax} | Y enveloppe: {ymin} - {ymax} | Z enveloppe: {zmin} - {zmax}"
+        )
+        print(f"Raw:{xmin},{xmax},{ymin},{ymax},{zmin},{zmax}")
+        printsep()
+
+        self.xCenter, self.yCenter, self.zCenter = (
+            (xmax + xmin) / 2,
+            (ymax + ymin) / 2,
+            (zmax + zmin) / 2,
+        )
+
+        ax_calib = self.fig.add_axes(
+            (
+                self.ax.get_position().x1 + self.constants["btn_width"],
+                self.constants["btn_y"],
+                self.constants["btn_width"],
+                self.constants["btn_height"],
+            )
+        )
+
+        self.btn_calib = widgets.Button(ax_calib, "Calibrate")
+        self.btn_calib.on_clicked(self.recenter)
+
+        self.fig.canvas.draw_idle()
+
+    def recenter(self, _):
+        if not self.xCenter or self.thread.is_alive():
+            return
+
+        self.requeueWOffset(self.xy_data, self.xCenter, self.yCenter)
+        self.requeueWOffset(self.yz_data, self.yCenter, self.zCenter)
+        self.requeueWOffset(self.zx_data, self.zCenter, self.xCenter)
+
+        print("Recalculated point positions")
+
+        self.update()
+
+
+def main():
+    choices = {}
+
+    scanports(choices)
+    printsep()
+
+    if len(choices) == 0:
+        print("No COM ports were found, aborting...")
+        sys.exit(0)
+
+    choice = promptuser(choices)
+
+    if choice:
+        print(f"Port {choice} was chosen")
+    else:
+        print("That COM port was not found, aborting...")
+        sys.exit(0)
+
+    mcu = serial.Serial(choice, BAUD_RATE, timeout=1)
+    compass = CompassGUI(mcu)
+
+    try:
+        plt.show(block=True)
+    except KeyboardInterrupt:
+        print("Interrupted, aborting...")
+    finally:
+        compass.is_running = False
+        if mcu.is_open:
+            mcu.close()
+
+
+if __name__ == "__main__":
+    main()

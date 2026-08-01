b90b69e6bcb90abba1d1f35b7d037d4a6089dd45
Proud Cobol Writer <77924245+proudCobolWriter@users.noreply.github.com>
2026-08-02 01:55:09 +0200

    Switch over to short ints to leverage the 12-bit accuracy of the ADC

--- sketches/JOYSTICK_Demo_Advanced/JOYSTICK_Demo_Advanced.ino
+++ sketches/JOYSTICK_Demo_Advanced/JOYSTICK_Demo_Advanced.ino
@@ -1,13 +1,22 @@
+// The following code could be used to improve the ADC accuracy, but in that case
+// the arbitrary unit analogRead returns should be dropped and mV/V used
+// https://github.com/G6EJD/ESP32-ADC-Accuracy-Improvement-function/blob/master/ESP32_ADC_Read_Voltage_Accurate.ino
+#include <cmath>
+
 #define X_AXIS_PIN 32
 #define Y_AXIS_PIN 33
 
+#define ADC_RESOLUTION 12 // Sets the ESP32 ADC resolution to 12-bit so (0-4095) range
+
+constexpr int16_t MID_POINT = static_cast<int16_t>(powf(2.0f, (float)ADC_RESOLUTION) / 2);
+
 constexpr uint8_t SAMPLE_SIZE = 10;
-typedef uint8_t r_arr[SAMPLE_SIZE];
+typedef int16_t r_arr[SAMPLE_SIZE];
 
 r_arr x_readings = {0};
 r_arr y_readings = {0};
 
-uint8_t calibration_offsets[2] = {0, 0};
+float calibration_offsets[2] = {0.0f, 0.0f};
 float calibration_scales[2] = {1.0f, 1.0f};
 
 void setup() {
@@ -21,8 +30,7 @@ void setup() {
 	pinMode(X_AXIS_PIN, INPUT);
 	pinMode(Y_AXIS_PIN, INPUT);
 
-	// Sets the ESP32 ADC resolution to 8-bit so (0-255) range
-	analogReadResolution(8);
+	analogReadResolution(ADC_RESOLUTION);
 
 	Serial.println(F("Get ready for the calibration in 2 seconds..."));
 
@@ -36,15 +44,15 @@ void setup() {
 void calibrate() {
 	Serial.println(F("Starting calibration. Actuate the joystick in all directions for 5 seconds"));
 
-	uint8_t x_min, x_max, y_min, y_max;
-	x_min = x_max = y_min = y_max = 128;
+	int16_t x_min, x_max, y_min, y_max;
+	x_min = x_max = y_min = y_max = MID_POINT;
 
 	uint32_t start_timestamp = millis();
 	uint16_t time_elapsed = 0;
 
 	while (time_elapsed < 5000) {
-		uint8_t x = analogRead(X_AXIS_PIN);
-		uint8_t y = analogRead(Y_AXIS_PIN);
+		int16_t x = analogRead(X_AXIS_PIN);
+		int16_t y = analogRead(Y_AXIS_PIN);
 
 		if (x < x_min) {
 			x_min = x;
@@ -73,43 +81,43 @@ void calibrate() {
 	Serial.println();
 
 	// the range of values taken by one side (positive or negative) of the axis
-	uint8_t x_avg_delta = (x_max - x_min) / 2;
-	uint8_t y_avg_delta = (y_max - y_min) / 2;
+	float x_avg_delta = (x_max - x_min) / 2.0f;
+	float y_avg_delta = (y_max - y_min) / 2.0f;
 
 	// prevents zero division
-	if (x_avg_delta == 0) {
-		x_avg_delta = 128;
+	if (x_avg_delta == 0.0f) {
+		x_avg_delta = MID_POINT;
 	}
 
-	if (y_avg_delta == 0) {
-		y_avg_delta = 128;
+	if (y_avg_delta == 0.0f) {
+		y_avg_delta = MID_POINT;
 	}
 
-	uint8_t x_mid = (x_max + x_min) / 2;
-	uint8_t y_mid = (y_max + y_min) / 2;
+	float x_mid = (x_max + x_min) / 2.0f;
+	float y_mid = (y_max + y_min) / 2.0f;
 
 	calibration_offsets[0] = x_mid;
 	calibration_offsets[1] = y_mid;
 
-	float x_scale = 128.0f / x_avg_delta;
-	float y_scale = 128.0f / y_avg_delta;
+	float x_scale = MID_POINT / x_avg_delta;
+	float y_scale = MID_POINT / y_avg_delta;
 
 	calibration_scales[0] = x_scale;
 	calibration_scales[1] = y_scale;
 
 	Serial.println(F("Calibration run complete!"));
 	Serial.println(F("---------------------------------------------------"));
-	Serial.printf("Middle at X: %d | Y: %d\n", x_mid, y_mid);
-	Serial.printf("Value ranges: X: %d wide | Y: %d wide\n", x_avg_delta * 2, y_avg_delta * 2);
+	Serial.printf("Middle at X: %.0f | Y: %.0f\n", x_mid, y_mid);
+	Serial.printf("Value ranges: X: %.0f wide | Y: %.0f wide\n", x_avg_delta * 2, y_avg_delta * 2);
 	Serial.printf("Scale factors: X: %.2f | Y: %.2f\n", x_scale, y_scale);
 }
 
 template<typename T>
 T clamp(const T v, const T min, const T max) {
-    return (v <= min) ? min : ((v >= max) ? max : v);
+	return (v <= min) ? min : ((v >= max) ? max : v);
 }
 
-void insert_at(r_arr arr, uint8_t n, uint8_t pos, uint8_t val) {
+void insert_at(r_arr arr, uint8_t n, uint8_t pos, int16_t val) {
 	if (n == 0 || pos >= n)
 		return;
 
@@ -124,7 +132,7 @@ float get_mean(const r_arr arr, uint8_t n) {
 		return 0.0f;
 	}
 
-	int16_t sum = 0;
+	int32_t sum = 0;
 	for (uint8_t i = 0; i < n; i++) {
 		sum += arr[i];
 	}
@@ -145,18 +153,9 @@ float get_variance(const r_arr arr, uint8_t n, float mean) {
 	return sumSquaredDifferences / (n - 1);
 }
 
-void applyOffsets(int8_t *x, int8_t *y) {
-    constexpr int8_t max_bound = 127;
-    constexpr int8_t min_bound = -128;
-
-    int8_t x_offseted = (*x) + calibration_offsets[0];
-    int8_t y_offseted = (*y) + calibration_offsets[1];
-
-    int8_t x_scaled = x_offseted * calibration_scales[0];
-    int8_t y_scaled = y_offseted * calibration_scales[1];
-
-	(*x) = x_scaled;//clamp<int8_t>(x_scaled, min_bound, max_bound);
-	(*y) = y_scaled;//clamp<int8_t>(y_scaled, min_bound, max_bound);
+void applyOffsets(int16_t *x, int16_t *y) {
+	(*x) = clamp<int16_t>(((*x) - calibration_offsets[0]) * calibration_scales[0], -MID_POINT, MID_POINT);
+	(*y) = clamp<int16_t>(((*y) - calibration_offsets[1]) * calibration_scales[1], -MID_POINT, MID_POINT);
 }
 
 void loop() {
@@ -175,8 +174,8 @@ void loop() {
 	float y_variance = get_variance(y_readings, SAMPLE_SIZE, y_mean);
 
 	Serial.printf("X:%d\nY:%d\n", x, y);
-	//Serial.printf("Average_X:%.2f\nAverage_Y:%.2f\n", x_mean, y_mean);
-	//Serial.printf("Variance_X:%.2f\nVariance_Y:%.2f\n", x_variance, y_variance);
+	Serial.printf("Average_X:%.2f\nAverage_Y:%.2f\n", x_mean, y_mean);
+	Serial.printf("Variance_X:%.2f\nVariance_Y:%.2f\n", x_variance, y_variance);
 
 	delay(100);
 }
